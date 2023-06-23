84ae5626b1c003853480c375e8e99690a879dc61
cezar <titicezar@gmail.com>
2023-06-24 00:26:05 +0300

    worked on data

--- link_volcanoes/main.py
+++ link_volcanoes/main.py
@@ -10,8 +10,8 @@ def main():
     volc_data = CloseVolcano(base_path, "volcanoes_mini.csv")
     
     file = open(base_path + 'data_linked.csv', 'w')
-    file.write("time,magnetometer,position,,volcano,,picture\n")
-    file.write("seconds,abs,latitude,longitude,name,distance,id\n")
+    file.write("time,magnetometer,position,,volcano,distance,coeficient,picture\n")
+    file.write("seconds,abs,latitude,longitude,name,radians,value,id\n")
 
     for snapshot in sorted_data:
         file.write(str(snapshot["time"]) + ',')
@@ -19,6 +19,8 @@ def main():
         file.write(str(snapshot["position"][0]) + ',' + str(snapshot["position"][1]) + ',')
         closest = volc_data.GetClose(snapshot["position"][0], snapshot["position"][1])
         file.write(closest[0] + ',' + str(closest[1]) + ',')
+        coef = volc_data.GetCoef(snapshot["position"][0], snapshot["position"][1])
+        file.write(str(coef) + ',')
         file.write(str(snapshot["picture"]) + ',')
         file.write('\n')
 
